f0293f75a73429dfd9259a011ed8bdaac4354cf3
fedPass <federica.passanante@gmail.com>
2020-02-17 17:26:56 +0100

    visualizzazione prodotti in index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,11 +8,35 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Lista prodotti</h1>
-                @forelse ($products as $product)
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">ID</th>
+                      <th scope="col">Nome</th>
+                      <th scope="col">Descrizione</th>
+                      <th scope="col">Prezzo</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                    @forelse ($products as $product)
+                          <tbody>
+                            <tr>
+                              <td>{{ $product->id }}</td>
+                              <td>{{ $product->name }}</td>
+                              <td>{{ $product->description }}</td>
+                              <td>{{ $product->price }}</td>
+                              <td>
+                                  <a href="#" class="btn btn-outline-success">Dettagli</a>
+                                  <a href="#" class="btn btn-success">Modifica</a>
+                                  <a href="#" class="btn btn-danger">Elimina</a>
+                              </td>
 
-                @empty
-                    <p>Non ci sono prodotti da visualizzare</p>
-                @endforelse
+                            </tr>
+                    @empty
+                        <p colspan="5">Non ci sono prodotti da visualizzare</p>
+                    @endforelse
+                    </tbody>
+                  </table>
             </div>
         </div>
     </div>
